27b6a86f9d92fcce4a4f7a8e4950bfb84d37c29d
rahil ahmadi <rahil.ahmadi2595@gmail.com>
2023-07-17 21:24:15 +0330

    change to better code and add button for open or close color pallet but have a problem in open or close

--- color pallet background_9/js/script.js
+++ color pallet background_9/js/script.js
@@ -1,3 +1,9 @@
 function changeColor(id){
     document.body.style.background=getComputedStyle(document.getElementById(id)).backgroundColor;
 }
+function openClose(){
+    let box=document.querySelector('.box');
+    let btn=document.getElementById('icon');
+    box.classList.toggle('active');
+    btn.classList.add('fas','fa-angle-up');
+}
