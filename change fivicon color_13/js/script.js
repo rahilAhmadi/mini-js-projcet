ec3486e6bdd67f860bbf3e0f0661427fdb24b639
rahil ahmadi <rahil.ahmadi2595@gmail.com>
2023-07-21 15:25:11 +0330

    write code for change favicon color

---
+++ change fivicon color_13/js/script.js
@@ -0,0 +1,7 @@
+const fivicon=document.getElementById('fivicon');
+function toGreen(){
+    fivicon.setAttribute('href','img/faviconTwo.png');
+}
+function toRed(){
+    fivicon.setAttribute('href','img/faviconOne.png');
+}
