44fcd9a3c531414d7053a84a5477f300e9eeedbf
rahil ahmadi <rahil.ahmadi2595@gmail.com>
2023-07-31 17:27:08 +0330

    create image slider when click on thumb change car image and change background color

---
+++ image slider_23/js/script.js
@@ -0,0 +1,7 @@
+function imageSlider(address){
+    document.querySelector('.bmw').src=address;
+}
+function changeBgColor(color){
+    const section=document.querySelector('.section');
+    section.style.background=color;
+}
