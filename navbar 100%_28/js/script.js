3817c43e2818acc2619746ca1187610fb78e9f61
rahil ahmadi <rahil.ahmadi2595@gmail.com>
2023-08-05 23:10:34 +0330

    create navbar 100 prject when scroll in page navbar's width is 100%

---
+++ navbar 100%_28/js/script.js
@@ -0,0 +1,13 @@
+let header=document.querySelector('.header')
+let sticky=header.offsetTop;
+
+
+
+window.addEventListener('scroll',()=>{
+    if(window.pageYOffset>sticky){
+        header.classList.add('sticky');
+    }
+    else{
+        header.classList.remove('sticky')
+    }
+})
