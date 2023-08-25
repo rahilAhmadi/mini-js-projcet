2b4a6b2b78cf564cbf710535adc46e46343528c3
rahil ahmadi <rahil.ahmadi2595@gmail.com>
2023-08-25 22:36:57 +0330

    add code for progress fix mouse

--- progress fix mouse_43/js/script.js
+++ progress fix mouse_43/js/script.js
@@ -2,11 +2,22 @@ const progressSection=document.querySelector('.progress-section');
 const progressBar=document.querySelector('.progress-bar');
 const progressNum=document.querySelector('.progress-num');
 
+let x,y;
+
+
+window.addEventListener('mousemove',function(e){
+    x=e.clientX;
+    y=e.clientY;
+
+})
+
 //innerHeight=ارتفاع کنونی
 //scrollHeight=ارتفاع کلی صفحه
 //window.scrollY=فاصله ای که اسکرول صفحه نسبت به بالای صفحه داره
 
 function updateProgressBar(){
+    progressSection.style.transform=`translate(${x}px,${y}px)`;
+    
     progressBar.style.height=`${Math.ceil(getScrollPrecentage())}%`;
     progressNum.innerText=`${Math.ceil(getScrollPrecentage())}%`
 
