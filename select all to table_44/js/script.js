d31f370eb7ad1c053e64ee0e710d26564bb884b4
rahil ahmadi <rahil.ahmadi2595@gmail.com>
2023-08-26 23:08:54 +0330

    update(select all to table_44):add remove all button and chenge to better code

--- select all to table_44/js/script.js
+++ select all to table_44/js/script.js
@@ -1,15 +1,25 @@
 const selectAll=document.getElementById('selectAll');
 const select=document.querySelectorAll('#select');
+const btn=document.querySelector('button');
 
 
 selectAll.onclick=()=>{
     if(selectAll.checked==true){
        for(i=0;i<select.length;i++){
             select[i].checked=true;
+            btn.classList.add('active');
        }
     }else{
         for(i=0;i<select.length;i++){
              select[i].checked=false;
+             btn.classList.remove('active');
         }
+    }
+}
+btn.onclick=()=>{
+    for(i=0;i<select.length;i++){
+        selectAll.checked=false;
+        select[i].checked=false;
+        btn.classList.remove('active');
     }
 }
