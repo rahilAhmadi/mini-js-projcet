63d0b74c7d54ea57fd994dec55de777939408580
rahil ahmadi <rahil.ahmadi2595@gmail.com>
2023-09-11 20:58:41 +0330

    update(zoom product_55): update css file and add js codes

--- zoom product_55/js/script.js
+++ zoom product_55/js/script.js
@@ -19,6 +19,25 @@ function imageZoomer() {
 
         let pos, x, y;
         pos = getCursorPosition(e);
+        x = pos.x - (span.offsetWidth / 2)
+        y = pos.y - (span.offsetHeight / 2)
+        if (x > img.width - span.offsetWidth) {
+            x = img.width - span.offsetWidth;
+        }
+        if(x<0){
+            x=0;
+        }
+        if (y > img.height - span.offsetHeight) {
+            y = img.height - span.offsetHeight;
+        }
+        if(y<0){
+            y=0;
+        }
+
+
+        span.style.left = x + 'px';
+        span.style.top = y + 'px';
+        div.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
 
         function getCursorPosition(e) {
             div.style.backgroundImage = "url('" + img.src + "')";
@@ -29,9 +48,17 @@ function imageZoomer() {
             a = img.getBoundingClientRect();
             x = e.pageX - a.left;
             y = e.pageY - a.top;
-            x = x - pageXOffset;
-            console.log(x);
+            x = x - window.pageXOffset;
+            y = y - window.pageYOffset;
+            return {
+                x: x,
+                y: y
+            }
         }
+    }
+    function leave(){
+        span.style.visibility='hidden';
+        div.style.visibility='hidden';
     }
 }
 
